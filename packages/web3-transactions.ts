71da9c01e75fe5aeb364aee7c4c3dafe1f0302c2
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-03-18 15:25:39 -0300

    refactor: change code structure

--- packages/web3-transactions.ts
+++ packages/web3-transactions.ts
@@ -36,26 +36,53 @@ import { TransactionReceipt } from 'web3-core';
  */
 
 export const sendTransactions = async (
+  fromAddress: string,
   fromPrivateKey: string,
   toAddress: string,
-  value: number
+  value: number,
+  gasFee?: string
 ) => {
+  const lastBlockValue: any = await web3Provider.eth.getBlock('latest');
+  const editGasFee =
+    gasFee === 'low' ? lastBlockValue * 0.8 : lastBlockValue * 1.2;
+
   const signedTransaction = await web3Provider.eth.accounts.signTransaction(
     {
+      from: fromAddress,
       to: toAddress,
       value: web3Provider.utils.toWei(value.toString(), 'ether'),
-      gas: await web3Provider.eth.estimateGas({
-        to: toAddress,
-      }),
+      gas: gasFee
+        ? editGasFee
+        : await web3Provider.eth.estimateGas({
+            to: toAddress,
+          }),
+      nonce: await web3Provider.eth.getTransactionCount(fromAddress, "latest")
     },
     fromPrivateKey
   );
 
+  console.log('EDIT GAS FEE', editGasFee);
+
+  console.log(
+    'NORMAL GAS FEE',
+    await web3Provider.eth.estimateGas({
+      to: toAddress,
+    })
+  );
+  console.log('nonce', await web3Provider.eth.getTransactionCount(fromAddress, "latest"))
+
   try {
     return web3Provider.eth
       .sendSignedTransaction(`${signedTransaction.rawTransaction}`)
-      .then((result: TransactionReceipt) => result);
+      .then((result: any) => result);
   } catch (error) {
     console.log(`${error}`);
   }
 };
+
+sendTransactions(
+  '0x0beaDdE9e116ceF07aFedc45a8566d1aDd3168F3',
+  '0x6e578c2227bc4629794e566610209c9cb7a35341f13de4ba886a59a4e11b7d1e',
+  '0xCe1812Ccc5273a3F8B1b2d96217877842a851A31',
+  0.01
+).then((result) => console.log(result));
